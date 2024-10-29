a73cb6f3d64ca47fb52676e5516d6cc6e54cc68d
Sayl0-404error <00001106829888sp@al.educacao.sp.gov.br>
2024-10-29 03:55:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao (
+    "astronomia",
+    "O que é universo?",
+    "Universo corresponde ao conjunto de toda a matéria, energia, espaço e tempo existente"
+)
+
+criaCartao (
+    "astronomia",
+    "Qual o maior corpo celeste do universo?",
+    "VY Canis Majoris, também conhecida como VY Cma. Sendo 2.100 vezes maior que o Sol em diâmetro"
+)
+
+criaCartao (
+    "astronomia",
+    "Qual é a velocidade da luz em km?",
+    "a cada segundo, a luz viaja aproximadamente 300.000 km"
+)
+
+criaCartao (
+    "fisica",
+    "Dois objetos de massas diferentes forem abandonados da mesma altura, quem cai primeiro?",
+    "As leis do movimento vertical de objetos mostram que o tempo de queda independe da massa dos corpos. Assim, se uma bola de boliche e uma pena de galinha forem abandonadas do mesmo ponto, ambas chegarão juntas ao solo"
+)
+
+criaCartao (
+    "fisica",
+    "Qual a formula do Movimento retilíneo uniforme?",
+    "s = s0 + v . ∆t .Onde s é a posição final (m) .s0 é a posição inicial (m) .v é a velocidade (m/s) e ∆t é o intervalo de tempo (s)")
+
+criaCartao (
+    "fisica",
+    "Formula da Velocidade Média?",
+    "Vm = ΔD/ΔT .Onde ΔD é a distância percorrida e ΔT é o intervalo de tempo"
+)
+
+criaCartao (
+    "química",
+    "número atômico",
+    "Quantidade de Prótons (Z)"
+
+)
+
+criaCartao (
+    "química",
+    "quantidade de massa",
+    "Prótons + Neutrons"
+    
+)
+
+criaCartao (
+    "química",
+    "quantidade de neutrons",
+    "Massa - Prótons"
+)
